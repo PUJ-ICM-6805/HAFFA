07b158107a14e96c8cbc989c084b8d9625b54c4e
FabioB8a <fabiobuitra@gmail.com>
2023-09-01 22:15:41 -0500

    Update views: Regist, Login and HomePage

--- app/src/main/java/com/example/haffa/MainActivity.kt
+++ app/src/main/java/com/example/haffa/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.haffa
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD
 import com.example.haffa.databinding.ActivityHomeLoginBinding
 import com.example.haffa.main.HomePageActivity
 import com.example.haffa.main.RegistryActivity
-=======
 import android.os.Bundle
 import com.example.haffa.journey.ShowAllJourneysActivity
->>>>>>> origin/main
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-<<<<<<< HEAD
         binding.logIn.setOnClickListener(){
             val intent = Intent(this, HomePageActivity::class.java)
             startActivity(intent)
@@ -33,12 +28,5 @@ class MainActivity : AppCompatActivity() {
 
         //val intent = Intent(this, HomePageActivity::class.java)
         //startActivity(intent)
-=======
-        setContentView(R.layout.activity_main)
->>>>>>> origin/main
-
-        //Test
-        val intent = Intent(this, ShowAllJourneysActivity::class.java)
-        startActivity(intent)
     }
 }
