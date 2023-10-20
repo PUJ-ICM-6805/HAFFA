777071eb7d5cc6708810b71ece564ed333323f0d
FabioB8a <fabiobuitra@gmail.com>
2023-10-20 00:26:13 -0500

    ADD: Comments DatePicker

--- app/src/main/java/com/example/haffa/utils/DatePicker.kt
+++ app/src/main/java/com/example/haffa/utils/DatePicker.kt
@@ -1,5 +1,3 @@
-package com.example.haffa.utils
-
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
@@ -8,8 +6,11 @@ import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import java.util.Calendar
 
-class DatePicker(private val dateEditText: EditText): DialogFragment(), DatePickerDialog.OnDateSetListener {
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+// This class, DatePicker, extends DialogFragment and implements DatePickerDialog.OnDateSetListener.
+class DatePicker(private val dateEditText: EditText) : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    // This function creates a new DatePickerDialog with the current date as the default date.
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         // Use the current date as the default date in the picker.
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
@@ -18,10 +19,11 @@ class DatePicker(private val dateEditText: EditText): DialogFragment(), DatePick
 
         // Create a new instance of DatePickerDialog and return it.
         return DatePickerDialog(requireContext(), this, year, month, day)
-
     }
 
+    // This function is called when the user sets a date using the date picker dialog.
     override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+        // Format the selected date and set it in the associated EditText.
         val selectedDate = String.format("%04d-%02d-%02d", year, month + 1, day)
         dateEditText.setText(selectedDate)
     }
