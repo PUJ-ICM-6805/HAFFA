85f842791a5ad76a6e71e8e115cf8f5daba4bf89
FabioB8a <fabiobuitra@gmail.com>
2023-10-20 00:28:45 -0500

    ADD: Comments Verification

--- app/src/main/java/com/example/haffa/utils/Verification.kt
+++ app/src/main/java/com/example/haffa/utils/Verification.kt
@@ -1,17 +1,18 @@
 package com.example.haffa.utils
 
-import android.content.Intent
 import android.text.TextUtils
 import android.widget.EditText
-import com.example.haffa.navigation.BottomNavigation
-import com.google.firebase.auth.FirebaseUser
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
+/**
+ * Utility class for verification and validation operations.
+ */
 class Verification {
+
     private val VALID_EMAIL_ADDRESS_REGEX =
         Pattern.compile("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
 
@@ -27,7 +28,14 @@ class Verification {
 
     private val VALID_PASSWD_REGEX = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?=.*[.!,]).{8,}\$")
 
-     fun validateForm(emailEdit: EditText, passEdit:EditText): Boolean {
+    /**
+     * Validates the input fields for email and password.
+     *
+     * @param emailEdit The email EditText field.
+     * @param passEdit The password EditText field.
+     * @return True if the fields are valid, false otherwise.
+     */
+    fun validateForm(emailEdit: EditText, passEdit: EditText): Boolean {
         var valid = true
         val email = emailEdit.text.toString()
         if (TextUtils.isEmpty(email)) {
@@ -46,34 +54,57 @@ class Verification {
         return valid
     }
 
-     fun isEmailValid(emailStr: String?): Boolean {
+    /**
+     * Checks if the given email address is valid.
+     *
+     * @param emailStr The email address to check.
+     * @return True if the email is valid, false otherwise.
+     */
+    fun isEmailValid(emailStr: String?): Boolean {
         val matcher: Matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr)
         return matcher.find()
     }
 
-    fun isTelephoneValid(telephone:String?): Boolean {
+    /**
+     * Checks if the given telephone number is valid.
+     *
+     * @param telephone The telephone number to check.
+     * @return True if the telephone number is valid, false otherwise.
+     */
+    fun isTelephoneValid(telephone: String?): Boolean {
         val matcher: Matcher = VALID_PHONE_NUMBER_REGEX.matcher(telephone)
         return matcher.find()
     }
+
+    /**
+     * Checks if the given password is valid. The password must contain at least one digit,
+     * one uppercase letter, one symbol (., !), and be at least 8 characters long.
+     *
+     * @param password The password to check.
+     * @return True if the password is valid, false otherwise.
+     */
     fun isPasswordValid(password: String?): Boolean {
         if (password == null) {
             return false
         }
-        val pattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[.!,]).{8,}\$"
-        val matcher: Matcher = Pattern.compile(pattern).matcher(password)
+        val matcher: Matcher = VALID_PASSWD_REGEX.matcher(password)
         return matcher.matches()
     }
 
+    /**
+     * Checks if the given date of birth corresponds to an adult. The date should be in "dd/MM/yyyy" format.
+     *
+     * @param dateOfBirth The date of birth to check.
+     * @return True if the date corresponds to an adult, false otherwise.
+     */
     fun isAdult(dateOfBirth: String): Boolean {
         if (dateOfBirth.isEmpty()) {
-            // Si la cadena de fecha está vacía, no podemos calcular la edad.
+            // If the date string is empty, we cannot calculate the age.
             return false
         }
 
         val dateFormat = SimpleDateFormat("dd/MM/yyyy")
-
         val currentDate = Date()
-
         val dateOfBirthDate = dateFormat.parse(dateOfBirth)
 
         val calendarCurrent = Calendar.getInstance()
@@ -86,6 +117,15 @@ class Verification {
         return age >= 18
     }
 
+    /**
+     * Validates various user data fields, including email, telephone, password, and birth date.
+     *
+     * @param emailEditText The email EditText field.
+     * @param telephoneEditText The telephone EditText field.
+     * @param passwordEditText The password EditText field.
+     * @param birthDateEditText The birth date EditText field.
+     * @return True if all fields are valid, false otherwise.
+     */
     fun isDataValid(
         emailEditText: EditText,
         telephoneEditText: EditText,
@@ -95,28 +135,27 @@ class Verification {
         var check = true
 
         if (!isEmailValid(emailEditText.text.toString())) {
-            emailEditText.error = "Formato de correo electrónico inválido"
+            emailEditText.error = "Invalid email format"
             check = false
         }
 
         if (!isTelephoneValid(telephoneEditText.text.toString())) {
-            telephoneEditText.error = "Formato teléfono inválido"
+            telephoneEditText.error = "Invalid phone format"
             check = false
         }
 
         if (passwordEditText.isEnabled && !isPasswordValid(passwordEditText.text.toString())) {
-            passwordEditText.error = "Recuerda colocar un número, una mayúscula y un símbolo (, . !)"
+            passwordEditText.error = "Must contain at least one digit, one uppercase letter, and one symbol (., !). Minimum length: 8"
             check = false
         }
 
         if (!isAdult(birthDateEditText.text.toString())) {
-            birthDateEditText.error = "Solamente se admiten mayores de edad"
+            birthDateEditText.error = "Only adults are allowed"
             check = false
         }
 
-        // Agregar verificación de username repetido
+        // TODO: Add username repetition verification here
 
         return check
     }
-
 }
